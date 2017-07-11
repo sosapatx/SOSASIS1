bb85b32483ef08dba0fd5d966fd4486bb609685a
vml03 <vicente.minero@itssmt.edu.mx>
2017-07-11 15:24:01 -0500

    ya funciona

--- resources/views/cliente/Ruta/index.blade.php
+++ resources/views/cliente/Ruta/index.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-		<h3>LISTADO DE RUTAS <a href="Ruta/create"><button class="btn btn-success">Nuevo</button></a></h3>
+		<h3>LISTADO DE RUTAS ya funciona el git<a href="Ruta/create"><button class="btn btn-success">Nuevo</button></a></h3>
 		@include('cliente.Ruta.search')
 	</div>
 </div>
